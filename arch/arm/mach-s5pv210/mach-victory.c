6a9c7df87bbaf8d2ea9b9862a2b388579ef28575
jt1134 <jt1134@gmail.com>
2012-10-23 21:26:34 -0500

    enable 1.2GHz OC
    * keep same voltage settings as 1GHz
    * properly update APLL values
    * set max freq to 1GHz on bootup, up to the user to OC
    
    Change-Id: I643cb1b54dd684b9dbe6066fbdef1a86b9123d79

--- arch/arm/mach-s5pv210/mach-victory.c
+++ arch/arm/mach-s5pv210/mach-victory.c
@@ -445,6 +445,10 @@ static struct s5p_media_device victory_media_devs[] = {
 #ifdef CONFIG_CPU_FREQ
 static struct s5pv210_cpufreq_voltage smdkc110_cpufreq_volt[] = {
 	{
+		.freq = 1200000,
+		.varm = 1275000,
+		.vint = 1125000,
+	}, {
 		.freq = 1000000,
 		.varm = 1275000,
 		.vint = 1100000,
