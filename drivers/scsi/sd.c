e7484bac077bc78107ad98fae5230a9845a9431a
Steve Magnani <smagnani@iscandar.digidescorp.com>
2013-04-12 20:44:48 +0000

    sd: fix infinite kernel/udev loop on non-removable Medium Not Present
    Commit eface65c336eff420d70beb0fb6787a732e05ffb (2.6.38) altered
    set_media_not_present() in a way that prevents the sd driver from
    remembering that a non-removable device has reported "Medium Not Present".
    This condition can occur on hotplug of a (i.e.) USB Mass Storage device
    whose medium is offline due to an unrecoverable controller error,
    but which is otherwise capable of SCSI communication (to download new
    microcode, etc.).
    
    Under these conditions, the changed code results in an infinite loop
    between the kernel and udevd. When udevd attempts to open the device
    in response to a change notification, a SCSI "Medium Not Present" error
    occurs which causes the kernel to signal another change. The cycle
    repeats until the device is unplugged, resulting in udevd consuming ever-
    increasing amounts of CPU and virtual memory.
    
    Resolve this by remembering "media not present" whether the device has
    declared itself "removable" or not.
    
    Signed-off-by: Steven J. Magnani <steve@digidescorp.com>

--- drivers/scsi/sd.c
+++ drivers/scsi/sd.c
@@ -1111,10 +1111,8 @@ out:
 
 static void set_media_not_present(struct scsi_disk *sdkp)
 {
-	if (sdkp->media_present)
+	if (sdkp->media_present) {
 		sdkp->device->changed = 1;
-
-	if (sdkp->device->removable) {
 		sdkp->media_present = 0;
 		sdkp->capacity = 0;
 	}
