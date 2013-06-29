dae6cf4444e95d1db60cf3f144f39b42c0381d08
didhiy <didhiy@gmail.com>
2013-06-30 06:08:36 +0700

    aries-panel: two lines from bbelos shadow kernel

--- arch/arm/mach-s5pv210/aries-panel.c
+++ arch/arm/mach-s5pv210/aries-panel.c
@@ -381,8 +381,8 @@ struct s5p_panel_data aries_panel_data = {
 		 * Bx 0.14142, By 0.04258
 		 */
 		.mult = {
-			2574671360U,
-			2770946560U,
+			2055448640U,
+			1932735200U,
 			2921039680U,
 		},
 		.rshift = 31,
